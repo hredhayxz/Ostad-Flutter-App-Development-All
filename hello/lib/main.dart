1d7db7b6f9c881242e9f8e2887d71274d86c91c0
Md Alhaz Mondal Hredhay <60058949+hredhayxz@users.noreply.github.com>
2023-08-11 18:46:18 +0600

    getx

--- hello/lib/main.dart
+++ hello/lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:hello/home_screen.dart';
+
 void main() {
-  runApp(MaterialApp(
-    home: Center(
-      child: Text("Hello world!"),
-    ),
-  ));
+  runApp(const MyCounterApp());
+}
+
+class MyCounterApp extends StatelessWidget {
+  const MyCounterApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      home: HomeScreen(),
+    );
+  }
 }

29eaa2d984f50ef59c5f101fb7b2f5a92b4591cd
Md Alhaz Mondal Hredhay <60058949+hredhayxz@users.noreply.github.com>
2023-08-16 21:28:53 +0600

    manually firebase connected done in android

--- firebase_test/lib/main.dart
+++ firebase_test/lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -11,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.

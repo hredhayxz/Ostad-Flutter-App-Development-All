006550b9c37e15189c7f22846c690ef6c163239f
Md Alhaz Mondal Hredhay <60058949+hredhayxz@users.noreply.github.com>
2023-08-22 18:16:01 +0600

    Null safety handled

--- firebase_test/lib/main.dart
+++ firebase_test/lib/main.dart
@@ -36,53 +36,59 @@ class HomeScreen extends StatelessWidget {
           builder:
               (context, AsyncSnapshot<DocumentSnapshot<Object?>> snapshot) {
             print(snapshot.data?.data());
-            final score = snapshot.data!;
-            return Center(
-              child: Column(
-                children: [
-                  const SizedBox(
-                    height: 48,
-                  ),
-                  Text(
-                    score.get('match_name'),
-                    style: Theme.of(context).textTheme.headline4,
-                  ),
-                  const SizedBox(
-                    height: 24,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Column(
-                        children: [
-                          Text(
-                            score.get('team_a_score').toString(),
-                            style: Theme.of(context).textTheme.titleLarge,
-                          ),
-                          Text(
-                            score.get('team_a'),
-                            style: Theme.of(context).textTheme.titleLarge,
-                          ),
-                        ],
-                      ),
-                      const Text('vs'),
-                      Column(
-                        children: [
-                          Text(
-                            score.get('team_b_score').toString(),
-                            style: Theme.of(context).textTheme.titleLarge,
-                          ),
-                          Text(
-                            score.get('team_b'),
-                            style: Theme.of(context).textTheme.titleLarge,
-                          ),
-                        ],
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            );
+            if (snapshot.hasData) {
+              final score = snapshot.data!;
+              return Center(
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 48,
+                    ),
+                    Text(
+                      score.get('match_name'),
+                      style: Theme.of(context).textTheme.headline4,
+                    ),
+                    const SizedBox(
+                      height: 24,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Column(
+                          children: [
+                            Text(
+                              score.get('team_a_score').toString(),
+                              style: Theme.of(context).textTheme.titleLarge,
+                            ),
+                            Text(
+                              score.get('team_a'),
+                              style: Theme.of(context).textTheme.titleLarge,
+                            ),
+                          ],
+                        ),
+                        const Text('vs'),
+                        Column(
+                          children: [
+                            Text(
+                              score.get('team_b_score').toString(),
+                              style: Theme.of(context).textTheme.titleLarge,
+                            ),
+                            Text(
+                              score.get('team_b'),
+                              style: Theme.of(context).textTheme.titleLarge,
+                            ),
+                          ],
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              );
+            } else {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
           }),
       floatingActionButton: FloatingActionButton(
         onPressed: () {

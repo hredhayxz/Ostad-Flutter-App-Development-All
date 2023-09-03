1656c5b9ce3f6825ef693d655bf1dd89d784e093
Md Alhaz Mondal Hredhay <60058949+hredhayxz@users.noreply.github.com>
2023-09-03 22:08:17 +0600

    test

---
+++ qproco_interview_test/test/qproco_interview_test_test.dart
@@ -0,0 +1,8 @@
+import 'package:qproco_interview_test/qproco_interview_test.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

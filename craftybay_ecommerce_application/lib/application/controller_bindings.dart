d4b8d64d07fbd4448ea4218bcaab13d7eba74569
Md Alhaz Mondal Hredhay <60058949+hredhayxz@users.noreply.github.com>
2023-09-25 21:28:53 +0600

    Email verification api integration has been added
    Email verification api integration has been added

--- craftybay_ecommerce_application/lib/application/controller_bindings.dart
+++ craftybay_ecommerce_application/lib/application/controller_bindings.dart
@@ -1,3 +1,4 @@
+import 'package:craftybay_ecommerce_application/presentation/state_holders/auth/email_verification_screen_controller.dart';
 import 'package:craftybay_ecommerce_application/presentation/state_holders/auth/otp_verification_screen_controller.dart';
 import 'package:craftybay_ecommerce_application/presentation/state_holders/main_bottom_nav_screen_controller.dart';
 import 'package:get/get.dart';
@@ -8,6 +9,7 @@ class ControllerBinding extends Bindings
   void dependencies() {
     Get.put(OtpVerificationScreenController());
     Get.put(MainBottomNavScreenController());
+    Get.put(EmailVerificationScreenController());
   }
 
 }

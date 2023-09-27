bf67be4ff14a44e7e8e0170901e1d52036323ff8
Md Alhaz Mondal Hredhay <60058949+hredhayxz@users.noreply.github.com>
2023-09-27 17:10:25 +0600

    otp verification api integration has been added

--- craftybay_ecommerce_application/lib/data/utility/urls.dart
+++ craftybay_ecommerce_application/lib/data/utility/urls.dart
@@ -4,4 +4,9 @@ class Urls {
   static const String _baseUrl = 'https://craftybay.teamrabbil.com/api';
 
   static String verifyEmail(String email) => '$_baseUrl/UserLogin/$email';
+
+  static String verifyOtp(String email, String otp) =>
+      '$_baseUrl/VerifyLogin/$email/$otp';
+
+  static String getHomeSliders = '$_baseUrl/ListProductSlider';
 }
--- craftybay_ecommerce_application/lib/presentation/ui/screens/splash_screen.dart
+++ craftybay_ecommerce_application/lib/presentation/ui/screens/splash_screen.dart
@@ -1,4 +1,6 @@
+import 'package:craftybay_ecommerce_application/presentation/ui/screens/auth/auth_controller.dart';
 import 'package:craftybay_ecommerce_application/presentation/ui/screens/auth/email_verification_screen.dart';
+import 'package:craftybay_ecommerce_application/presentation/ui/screens/main_bottom_nav_screen.dart';
 import 'package:craftybay_ecommerce_application/presentation/ui/widgets/craftyBay_logo.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -19,7 +21,9 @@ class _SplashScreenState extends State<SplashScreen> {
 
   void goToNextScreen() {
     Future.delayed(const Duration(seconds: 2)).then((value) {
-      Get.offAll(() => EmailVerificationScreen());
+      Get.offAll(() => AuthController.isLoggedIn
+          ? const MainBottomNavScreen()
+          : EmailVerificationScreen());
     });
   }
 

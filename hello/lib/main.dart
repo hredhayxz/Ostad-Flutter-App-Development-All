aef444dfb12d0dd434f0dc8f4970191e2dc68514
Md Alhaz Mondal Hredhay <60058949+hredhayxz@users.noreply.github.com>
2023-10-08 21:35:01 +0600

    commit

--- hello/lib/main.dart
+++ hello/lib/main.dart
@@ -1,112 +1,131 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:craftybay_ecommerce_application/presentation/state_holders/category_controller.dart';
-import 'package:craftybay_ecommerce_application/presentation/state_holders/category_product_list_controller.dart';
-import 'package:craftybay_ecommerce_application/presentation/ui/screens/product_list_screen.dart';
-import 'package:craftybay_ecommerce_application/presentation/ui/widgets/category_card.dart';
-import 'package:craftybay_ecommerce_application/presentation/ui/widgets/home/home_screen_appbar_title.dart';
-import 'package:craftybay_ecommerce_application/presentation/ui/widgets/home/home_slider.dart';
-import 'package:craftybay_ecommerce_application/presentation/ui/widgets/product_listview.dart';
-import 'package:craftybay_ecommerce_application/presentation/ui/widgets/home/section_header.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        title: const HomeScreenAppbarTitle(),
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color.fromRGBO(35, 42, 43, 1),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              TextField(
-                decoration: InputDecoration(
-                  fillColor: Colors.grey.shade200,
-                  filled: true,
-                  prefixIcon: const Icon(Icons.search),
-                  hintText: 'Search',
-                  border: const OutlineInputBorder(borderSide: BorderSide.none),
-                  focusedBorder:
-                      const OutlineInputBorder(borderSide: BorderSide.none),
-                  enabledBorder:
-                      const OutlineInputBorder(borderSide: BorderSide.none),
-                ),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              GetBuilder<HomeSlidersController>(
-                  builder: (homeSliderController) {
-                if (homeSliderController.getHomeSlidersInProgress) {
-                  return const SizedBox(
-                    height: 180.0,
-                    child: Center(
-                      child: CircularProgressIndicator(),
+      home: SafeArea(
+        child: Scaffold(
+          body: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: 100,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Icon(Icons.delete),
+                        const VerticalDivider(
+                          color: Colors.grey,
+                        ),
+                        Column(
+                          children: [
+                            RichText(
+                              text: const TextSpan(
+                                text: "Meta",
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.amber,
+                                ),
+                                children: [
+                                  TextSpan(text: "  "),
+                                  TextSpan(
+                                    text: "Collaboration",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            RichText(
+                              text: const TextSpan(
+                                text: "With",
+                                style: TextStyle(fontSize: 18),
+                                children: [
+                                  TextSpan(text: "  "),
+                                  TextSpan(
+                                    text: "Pranayama",
+                                    style: TextStyle(
+                                      color: Colors.amber,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
                     ),
-                  );
-                }
-                return HomeSlider(
-                  sliders: homeSliderController.sliderModel.data ?? [],
-                );
-              }),
-              SectionHeader(
-                title: 'All Categories',
-                onTap: () {
-                  Get.find<MainBottomNavScreenController>().changeScreen(1);
-                },
-              ),
-              const SizedBox(
-                height: 8,
-              ),
-              GetBuilder<CategoryController>(
-                builder: (categoryController) {
-                  if (categoryController.getCategoriesInProgress) {
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  }
-                  return SizedBox(
-                    height: 90,
-                    child: ListView.builder(
-                      itemCount: categoryController.categoryModel.data?.length ?? 0,
-                      scrollDirection: Axis.horizontal,
-                      itemBuilder: (context, index) {
-                        final categoryProductListController =
-                            Get.put(CategoryProductListController()); // Create controller
-                        return GestureDetector(
-                          onTap: () async {
-                            await categoryProductListController
-                                .getProductsByCategory(index + 1);
-                            Get.to(() => ProductListScreen(
-                                productData:
-                                    categoryProductListController.productModel
-                                            .data ??
-                                        [],
-                                remarkName: categoryController.categoryModel
-                                        .data![index].categoryName ??
-                                    ''));
-                          },
-                          child: CategoryCard(
-                            categoryData:
-                                categoryController.categoryModel.data![index],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Card(
+                    elevation: 10,
+                    child: Container(
+                      height: 300,
+                      width: double.infinity,
+                      decoration: const BoxDecoration(
+                        color: Color.fromRGBO(47, 58, 59, 1),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Container(
+                              height: 50,
+                              width: double.infinity,
+                              color: Colors.red,
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Text(
+                    "You are now using",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  RichText(
+                    text: const TextSpan(
+                      text: "Pranayama",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
+                      children: [
+                        TextSpan(text: "  "),
+                        TextSpan(
+                          text: "Plus+",
+                          style: TextStyle(
+                            fontFamily: "Pacifico",
+                            color: Colors.amber,
                           ),
-                        );
-                      },
+                        ),
+                      ],
                     ),
-                  );
-                },
+                  ),
+                  const Text('End: 18 july 2025',
+                      style: TextStyle(color: Colors.white)),
+                ],
               ),
-              const SizedBox(
-                height: 16,
-              ),
-              // ... Continue with other sections and GetBuilder widgets
-            ],
+            ),
           ),
         ),
       ),
